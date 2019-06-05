f281d80e3bc7ead8dc1bafb3431bfef2207a6139
Dr-Salcedo <pablosalcedo@verizon.net>
2019-06-05 09:02:40 -0400

    Initialize repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A Statistical Analysis on  Mental Health Resources and Worldwide Suicide Rates',
+    author='pablo salcedo',
+    #license='',
+)
