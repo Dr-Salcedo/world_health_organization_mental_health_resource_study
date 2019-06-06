0f05e76cc249b17fe41fb9405112712cbfc23b99
Dr-Salcedo <pablosalcedo@verizon.net>
2019-06-06 16:46:21 -0400

    update

--- src/custom.py
+++
@@ -1,9 +0,0 @@
-# custom.py
-##----------
-## This is where you put your custom code that doesn't
-## fit the standard categories of data cleaning, feature engineering,
-## modeling, or visualization.
-
-def test_custom():
-    print('In custom module')
-    return None
